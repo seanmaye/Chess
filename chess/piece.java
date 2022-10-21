a437dd6ef426fed979bcfde3b9f80eded561fa7d
DeYiC <DeYiC@172.31.77.245>
2022-10-21 01:06:00 -0400

    updated piece class

--- chess/piece.java
+++ chess/piece.java
@@ -1,12 +1,30 @@
 package chess;
 
-public class Piece {
+public abstract class Piece {
 	private int x;
 	private int y;
 	
-	public Piece(int x, int y) {
+	public Piece (int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 	
+	public abstract void move();
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setX(int x) {
+		x = this.x;
+	}
+	
+	public void setY(int y) {
+		y = this.y;
+	}
+
 }
