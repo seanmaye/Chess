be308e1ba59bb8b1fb575f61b320171dd85b2967
DeYiC <DeYiC@172.31.79.113>
2022-10-31 01:49:31 -0400

    queen move & piece detection- all pieces functional now

--- src/chess/Queen.java
+++ src/chess/Queen.java
@@ -12,13 +12,101 @@ public class Queen extends Piece {
 			return false;
 		}
 		
-		
-		//rook code
 		if (dX==x && dY==y) {
 			return false;
 		}
 		
 		
+		//bishop code
+		boolean hit  = false;
+		boolean clear = true;
+		
+		for (int i = x+1, j = y+1; i<dX && j<dY; i++, j++) {
+			if (i==dX && j==dY) {
+				hit = true;
+			}
+			if (!(Board.board[i][j] instanceof Empty)){
+				clear = false;
+			}
+		}
+		
+		if (hit == true) {
+			if (clear == true) {
+				if(Board.board[dX][dY] instanceof Empty) {
+					return true;
+				} else if (!(Board.board[dX][dY] instanceof Empty) && (Board.board[dX][dY].getColor()!= color)){
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		
+		
+		clear = true;
+		for (int i = x-1, j = y-1; i>=0 && j>=0; i--, j--) {
+			if (i==dX && j==dY) {
+				hit = true;
+			}
+			if (!(Board.board[i][j] instanceof Empty)){
+				clear = false;
+			}
+		}
+		
+		if (hit == true) {
+			if (clear == true) {
+				if(Board.board[dX][dY] instanceof Empty) {
+					return true;
+				} else if (!(Board.board[dX][dY] instanceof Empty) && (Board.board[dX][dY].getColor()!= color)){
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		
+		clear = true;
+		for (int i = x+1, j = y-1; i<8 && j>=0; i++, j--) {
+			if (i==dX && j==dY) {
+				hit = true;
+			}
+			if (!(Board.board[i][j] instanceof Empty)){
+				clear = false;
+			}
+		}
+		
+		if (hit == true) {
+			if (clear == true) {
+				if(Board.board[dX][dY] instanceof Empty) {
+					return true;
+				} else if (!(Board.board[dX][dY] instanceof Empty) && (Board.board[dX][dY].getColor()!= color)){
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		
+		clear = true;
+		for (int i = x-1, j = y+1; i>=0 && j<8; i--, j++) {
+			if (i==dX && j==dY) {
+				hit = true;
+			}
+			if (!(Board.board[i][j] instanceof Empty)){
+				clear = false;
+			}
+		}
+		
+		if (hit == true) {
+			if (clear == true) {
+				if(Board.board[dX][dY] instanceof Empty) {
+					return true;
+				} else if (!(Board.board[dX][dY] instanceof Empty) && (Board.board[dX][dY].getColor()!= color)){
+					return true;
+				}
+			}
+			return false;
+		}
 		if (dX==x) {
 			return true;
 		}
@@ -29,49 +117,54 @@ public class Queen extends Piece {
 		
 		
 		
-		//bishop code
-		//boolean clear = true;
-				for (int i = x+1, j = y+1; i<8 && j<8; i++, j++) {
-					//if (!(i==dX && j==dY)&& !(Board.board[i][j] instanceof Empty)){
-						//clear = false;
-					//}
-					if (i==dX && j==dY) { //&& clear == true
-						return true;
-					}
-				}
-				
-				//clear = true;
-				for (int i = x-1, j = y-1; i>=0 && j>=0; i--, j--) {
-					//if (!(i==dX && j==dY)&& !(Board.board[i][j] instanceof Empty)){
-						//clear = false;
-					//}
-					if (i==dX && j==dY) {
-						return true;
-					}
-				}
-				
-				//clear = true;
-				for (int i = x+1, j = y-1; i<8 && j>=0; i++, j--) {
-					//if (!(i==dX && j==dY)&& !(Board.board[i][j] instanceof Empty)){
-						//clear = false;
-					//}
-					if (i==dX && j==dY) {
-						return true;
-					}
-				}
-				
-				//clear = true;
-				for (int i = x-1, j = y+1; i>=0 && j<8; i--, j++) {
-					//if (!(i==dX && j==dY)&& !(Board.board[i][j] instanceof Empty)){
-						//clear = false;
-					//}
-					if (i==dX && j==dY) {
-						return true;
-					}
-				}
+		//rook code
+		if (!(dX==x || dY==y)) {
+			return false;
+		}
 		
-		return false;
-
+		
+		if (dX==x) {
+			if (dY > y) {
+				for (int j = y+1; j<dY; j++) {
+					if (!(Board.board[dX][j] instanceof Empty)) {
+						return false;
+					}
+				}
+			} else { //(dY < y)
+				for (int j = y-1; j>dY; j--) {
+					if (!(Board.board[dX][j] instanceof Empty)) {
+						return false;
+					}
+				}
+			}
+		}
+			
+				
+		if (dY==y) {
+			if (dX > x) {
+				for (int i = x+1; i<dX; i++) {
+					if (!(Board.board[i][dY] instanceof Empty)) {
+						return false;
+					}
+				}
+			} else { //(dX < x
+				for (int i = x-1; i>dX; i--) {
+					if (!(Board.board[i][dY] instanceof Empty)) {
+						return false;
+					}
+				}
+			}
+		}
+		
+		if (Board.board[dX][dY] instanceof Empty) {
+			return true;
+		} else {
+			if (Board.board[dX][dY].getColor()== color) {
+				return false;
+			} else {
+				return true;
+			}
+		}
 	}
 
 	public String toString() {
