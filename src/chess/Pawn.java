adab8236b5f86b345e43c13f17502c801e123375
seanmaye <seanmaye258@gmail.com>
2022-11-01 16:39:56 -0400

    en passant done?

--- src/chess/Pawn.java
+++ src/chess/Pawn.java
@@ -2,11 +2,12 @@ package chess;
 
 public class Pawn extends Piece {
 	private boolean isFirstMove;
+	public int move;
 
 	public Pawn(int x, int y, boolean color) {
 		super(x, y, color);
 		isFirstMove = true;
-
+		move=0;
 	}
 
 	public boolean move(int dX, int dY) {
@@ -20,38 +21,124 @@ public class Pawn extends Piece {
 		
 		
 		if (color) {
-			if ((dX == x-2 && dY == y) && isFirstMove == true
+			//System.out.println(x +" " +y);
+			if(x==3 &&Board.board[x][y-1]instanceof Pawn) {
+				Pawn toEnpassant = (Pawn) Board.board[x][y-1];
+				
+				if (toEnpassant.move==1) {
+					move++;
+					if ((y-1) % 2 == 0) {
+						Board.board[x][y-1] = new Empty(x, y-1, false);
+					} else {
+						Board.board[x][y-1] = new Empty(x, y-1, true);
+					}
+					if ((y-1)% 2 == 1) {
+						Board.board[x][y-1] = new Empty(x, y-1, false);
+					} else {
+						Board.board[x][y-1] = new Empty(x, y-1, true);
+					}
+					return true;
+				}else {
+					return false;
+				}
+			}else if(x==3 &&Board.board[x][y+1]instanceof Pawn) {
+				Pawn toEnpassant = (Pawn) Board.board[x][y+1];
+				
+				if (toEnpassant.move==1) {
+					move++;
+					if ((y+1) % 2 == 0) {
+						Board.board[x][y+1] = new Empty(x, y+1, false);
+					} else {
+						Board.board[x][y+1] = new Empty(x, y+1, true);
+					}
+					if ((y+1)% 2 == 1) {
+						Board.board[x][y+1] = new Empty(x, y+1, false);
+					} else {
+						Board.board[x][y+1] = new Empty(x, y+1, true);
+					}
+					return true;
+				}else {
+					return false;
+				}
+			}else if ((dX == x-2 && dY == y) && isFirstMove == true
 					&& Board.board[dX][dY]instanceof Empty && Board.board[x-1][y] instanceof Empty) {
 				//x = x - 2;
-				isFirstMove = false;
+		 		isFirstMove = false;
+		 		move++;
 				return true;
 			} else if (dX == x-1 && dY == y
 					&& Board.board[dX][dY] instanceof Empty) {
 				//x = x - 1;
 				isFirstMove = false;
+				move++;
 				return true;
 			} else if (((dX == x-1 && dY == y-1) || (dX == x-1 && dY == y+1))
 					&& ((!(Board.board[dX][dY] instanceof Empty) && (Board.board[dX][dY].getColor() != color)))){
 				isFirstMove = false;
+				move++;
 				return true;
 			} else {
 				return false;
 			}
 		} else {
-			if ((dX == x+2 && dY == y) && isFirstMove == true
+			if(x==4 &&Board.board[x][y-1]instanceof Pawn) {
+				Pawn toEnpassant = (Pawn) Board.board[x][y-1];
+				
+				if (toEnpassant.move==1) {
+					move++;
+					if ((y-1) % 2 == 0) {
+						Board.board[x][y-1] = new Empty(x, y-1, true);
+					} else {
+						Board.board[x][y-1] = new Empty(x, y-1, false);
+					}
+					if ((y-1)% 2 == 1) {
+						Board.board[x][y-1] = new Empty(x, y-1, true);
+					} else {
+						Board.board[x][y-1] = new Empty(x, y-1, false);
+					}
+					return true;
+				}else {
+					return false;
+				}
+			}else if(x==4 &&Board.board[x][y+1]instanceof Pawn) {
+				Pawn toEnpassant = (Pawn) Board.board[x][y+1];
+				
+				if (toEnpassant.move==1) {
+					move++;
+					if ((y+1) % 2 == 0) {
+						Board.board[x][y+1] = new Empty(x, y+1, true);
+						
+					} else {
+						Board.board[x][y+1] = new Empty(x, y+1, false);
+					}
+					if ((y+1)% 2 == 1) {
+						Board.board[x][y+1] = new Empty(x, y+1, true);
+					} else {
+						Board.board[x][y+1] = new Empty(x, y+1, false);
+						
+					}
+					return true;
+				}else {
+					return false;
+				}
+			}else if ((dX == x+2 && dY == y) && isFirstMove == true
 					&& Board.board[dX][dY] instanceof Empty && Board.board[x+1][y] instanceof Empty) {
 				//x = x + 2;
 				isFirstMove = false;
+				move++;
 				return true;
 			} else if (dX == x+1 && dY == y
 					&& Board.board[dX][dY] instanceof Empty) {
 				//x = x + 1;
 				isFirstMove = false;
+				move++;
 				return true;
 			} else if (((dX == x+1 && dY == y-1) || (dX == x+1 && dY == y+1))
 					&& ((!(Board.board[dX][dY] instanceof Empty) && (Board.board[dX][dY].getColor() != color)))){
 				isFirstMove = false;
+				move++;
 				return true;
+				
 			} else {
 				return false;
 			}
@@ -59,13 +146,16 @@ public class Pawn extends Piece {
 
 
 	}
+	
 
 	public String toString() {
+		
 		if (color) {
 			return "wp";
 		} else {
 			return "bp";
 		}
+		}
+
 	}
 
-}
